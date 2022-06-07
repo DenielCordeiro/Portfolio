cfd1838a76df1ed4721b4c521982d813c63bca54
Daniel de Souza Cordeiro <dcordeiro962@gmail.com>
2022-06-07 10:42:03 -0300

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Lyrics
+# Portfolio
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.0.0.
 
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/lyrics'),
+      dir: require('path').join(__dirname, './coverage/portfolio'),
       subdir: '.',
       reporters: [
         { type: 'html' },
