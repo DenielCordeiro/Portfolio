57d30d0f09058110012ca697249dceb7c2afa3da
Daniel de Souza Cordeiro <dcordeiro962@gmail.com>
2022-06-07 11:04:15 -0300

    initial components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GreetingsComponent } from './greetings/greetings.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GreetingsComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/greetings/greetings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-greetings',
+  templateUrl: './greetings.component.html',
+  styleUrls: ['./greetings.component.sass']
+})
+export class GreetingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
