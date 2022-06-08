2c44a2cf4524b91f90d6803af714c1e0bcb85b25
Daniel de Souza Cordeiro <dcordeiro962@gmail.com>
2022-06-08 01:13:25 -0300

    create app-routing and new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HomeComponent } from './home/home.component';
+import { GreetingsComponent } from './greetings/greetings.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'header', component: HeaderComponent },
+  { path: 'sidebar', component: SidebarComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'greetings', component: GreetingsComponent },
+  { path: 'about', component: AboutComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
