c60320971de6c355a2139763233b44bfef3ff43c
Daniel de Souza Cordeiro <dcordeiro962@gmail.com>
2022-07-07 01:24:24 -0300

    remove header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppComponent } from './app.component';
 import { GreetingsComponent } from './greetings/greetings.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
-import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { FooterComponent } from './footer/footer.component';
 import { ProjectsComponent } from './projects/projects.component';
@@ -17,7 +16,6 @@ import { ProjectsComponent } from './projects/projects.component';
     GreetingsComponent,
     AboutComponent,
     HomeComponent,
-    HeaderComponent,
     SidebarComponent,
     FooterComponent,
     ProjectsComponent
