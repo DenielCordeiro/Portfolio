24012f05cea65039d314a4985560080c1253ffe9
Daniel de Souza Cordeiro <dcordeiro962@gmail.com>
2022-11-08 12:11:29 -0300

    creating components for home

---
+++ src/app/home/designs/designs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-designs',
+  templateUrl: './designs.component.html',
+  styleUrls: ['./designs.component.sass']
+})
+export class DesignsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
