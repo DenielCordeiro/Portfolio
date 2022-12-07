f4e2ef8ab04867e93800c12850e58f70e09666b1
daniel <dcordeiro962@gmail.com>
2022-12-07 12:58:42 -0300

    form reactived

--- src/app/home/contact/contact-modal/contact-modal.component.ts
+++ src/app/home/contact/contact-modal/contact-modal.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-modal',
@@ -7,16 +8,30 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./contact-modal.component.sass']
 })
 export class ContactModalComponent implements OnInit {
+  form!: FormGroup;
   urlToJson = 'assets/pt.json';
   result: any;
 
-  constructor( public http: HttpClient ) {
+  constructor(
+    public http: HttpClient,
+    public formBuilder: FormBuilder
+  ) {
     this.http.get<any>(this.urlToJson).subscribe(response => {
       this.result = response;
     });
    }
 
   ngOnInit(): void {
+    // alert()
+    this.formBuilding()
+  }
+
+  formBuilding(): void {
+    this.form = this.formBuilder.group({
+      "name": [null, Validators.required],
+      "email": [null, [Validators.required, Validators.email]],
+      "message": [null, Validators.required]
+    });
   }
 
 }
--- src/app/home/contact/contact.component.ts
+++ src/app/home/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -7,17 +8,29 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./contact.component.sass']
 })
 export class ContactComponent implements OnInit {
+  form!: FormGroup;
   urlToJson = 'assets/pt.json';
   result: any;
   linkedin: string = 'https://www.linkedin.com/in/daniel-de-souza-cordeiro-020915198/';
 
-  constructor( public http: HttpClient ) {
+  constructor(
+    public http: HttpClient,
+    public formBuilder: FormBuilder
+  ) {
     this.http.get<any>(this.urlToJson).subscribe(response => {
       this.result = response;
     });
    }
 
   ngOnInit(): void {
+    this.formBuilding();
   }
 
+  formBuilding(): void {
+    this.form = this.formBuilder.group({
+      "name": [null, Validators.required],
+      "email": [null, [Validators.required, Validators.email]],
+      "message": [null, Validators.required]
+    });
+  }
 }
